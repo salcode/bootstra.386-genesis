1d8732ece2804c9e05d9efa41bae6e8849563d8f
Sal Ferrarello <salcode@salogic.net>
2014-07-03 23:52:09 -0400

    update footer creds for bootstra.386

--- lib/footer-creds.php
+++ lib/footer-creds.php
@@ -3,6 +3,6 @@
 add_filter('genesis_footer_creds_text', 'bsg_footer_creds_filter');
 
 function bsg_footer_creds_filter( $creds ) {
-    $creds = '<a title="Bootstrap Genesis Theme" href="http://ironcodestudio.com/bootstrap-genesis-theme/">Bootstrap Genesis Theme</a> by <a href="http://ironcodestudio.com/">Iron Code Studio</a>';
+    $creds = '<a title="Bootstra.386 Bootstrap Genesis Theme" href="https://github.com/salcode/bootstra.386-genesis">Bootstra.386 Genesis Theme</a>';
     return $creds;
 }
